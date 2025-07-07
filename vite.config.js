92b031027407d8eb9e95ac035e2553bda061e0ee
clinton <ezeobiclinton@gmail.com>
2025-07-07 11:40:04 +0100

    Fix Vite base path for relative asset loading

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/job-app/',       // ← Important: Must match your GitHub repo name
+  base: './',       // ← Important: Must match your GitHub repo name
   plugins: [react()],
   server: {
     proxy: {
