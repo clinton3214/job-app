8b9fe869c8047553ca245dd7c5fd759db8fd9e3b
clinton <ezeobiclinton@gmail.com>
2025-08-02 16:15:06 +0100

    Initial

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -318,7 +318,7 @@ export default function DashboardPage() {
       onClick={() => navigate(path)}
       initial={{ opacity: 0, y: 40 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.3 }}
+      viewport={{ once: false, amount: 0.3 }}
       transition={{ duration: 0.5, delay: idx * 0.1 }}
     >
       <div className="row g-0">
